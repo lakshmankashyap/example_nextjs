a5b8b5d7155d4a93066cec716b059564d3d92b90
lakshmankashyap <lkashyap92@gmail.com>
2021-11-18 11:18:56 +0530

    isr add

---
+++ pages/isr.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import TimeSection from '../components/timesection';
+
+export default function ISRPage() {
+    const [dateTime, setDateTime] = useState();
+    useEffect(() => {
+        fetch("https://worldtimeapi.org/api/ip")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setDateTime(result.datetime);
+                },
+                (error) => {
+                    console.error(error);
+                }
+            )
+    }, []);
+    return (
+        <TimeSection dateTime={dateTime} />
+    );
+}
+
+export const getStaticProps = async () => {
+    const res = await fetch("https://worldtimeapi.org/api/ip").then(res => res.json())
+
+    return {
+        props: { dateTime: res.datetime },
+        revalidate: 20,
+    }
+}
